993faa75d2284f995ef6e0435aa83ad7bd592674
Lucía Mondéjar <luciia.m93@gmail.com>
2019-03-14 12:01:49 +0100

    Ejercicios 1 y 3

---
+++ 01 Python/03_personal-info.py
@@ -0,0 +1,12 @@
+#Crear un script que pregunte por el nombre, el apellido, el teléfono, la ciudad, y la edad.
+#Cada valor ha de ser guardado en una variable diferente.
+#Imprimir, por pantalla, toda la infomarción coleccionada en una frase que combine la información
+#con coherencia (una oración que no sólo sean los datos recogidos).
+
+first_name = input()
+last_name = input()
+phone = input()
+location = input()
+age = input()
+
+print("Me llamo " + first_name + last_name + ", vivo en" + location + "y tengo " + age + "años. Mi número de teléfono es " + phone)
