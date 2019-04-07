c151473c9c2712c81e3a2f9aa1ebf3c952a89ce5
Lucía Mondéjar <luciia.m93@gmail.com>
2019-04-07 21:06:51 +0200

    Calculadora realizada con diccionarios.

--- 01 Python/05_calculator.py
+++ 01 Python/05_calculator.py
@@ -26,29 +26,15 @@ def calculadora():
     #Permitir escoger el modo de operación de forma manual (el usuario ha de introducir un número para que sepa qué operación realizar)
     op = int(input("Escoja operación:\n 1.- Suma\n 2.- Resta\n 3.- Multiplicación\n 4.- División\n 5.- Exponencial\n"))
 
+    operaciones = {1: suma(), 2: resta(), 3: multiplicacion(), 4: division(), 5: exponencial()}
+    simbolo = {1: ' + ', 2: ' - ', 3: ' * ', 4: ' / ', 5: " ^ "}
 
-    #Realizar las operaciones e imprimir el valor por pantalla.
-    if op == 1:
-      print("Suma: " + str(a) + " + " + str(b) + " = " + str(suma()))
-    elif op == 2:
-      print("Resta: " + str(a) + " - " + str(b) + " = " + str(resta()))
-    elif op == 3:
-      print("Multiplicación: " + str(a) + " * " + str(b) + " = " + str(multiplicacion()))
-    elif op == 4:
-      try:
-        print("División: " + str(a) + " / " + str(b) + " = " + str(division()))
-      except ZeroDivisionError:
-        print("ERROR: No se puede dividir entre cero")
-    elif op == 5:
-      if b == 2:
-        print("Exponencial: " + str(a) + " elevado al cuadrado = " + str(exponencial()))
-      elif b == 3:
-        print("Exponencial: " + str(a) + " elevado al cubo = " + str(exponencial()))
-      else:
-        print("Exponencial: " + str(a) + " elevado a " + str(b) + " = " + str(exponencial()))
+    if operaciones[op] == 5 or b == 2:
+      print(str(a) + " al cuadrado = " + str(operaciones[op]))
+    elif (b == 3):
+      print(str(a) + " al cubo = " + str(operaciones[op]))
     else:
-      print("Operación no válida. Inténtelo de nuevo")
-      calculadora()
+      print(str(a) + simbolo[op] + str(b) + " = " + str(operaciones[op]))
   except ValueError:
     print("Por favor, inserte un número")
     calculadora()
