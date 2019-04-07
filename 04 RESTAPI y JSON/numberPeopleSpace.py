1e9c43fd11355050a6815992efdbd6a77cf1b9bb
Lucía Mondéjar <luciia.m93@gmail.com>
2019-04-07 20:32:54 +0200

    Eliminación de la carpeta de los enunciados RESTAPI y JSON.

--- 04 RESTAPI y JSON/numberPeopleSpace.py
+++
@@ -1,4 +0,0 @@
-#Tarea 2: Crear una APP para el ISS (International Space Station)
-
-#Crear una app para saber el número de personas que hay en el espacio. Podéis buscar la información en la siguiente URL:
-#http://open-notify.org/Open-Notify-API/
